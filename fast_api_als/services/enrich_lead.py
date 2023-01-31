28835fe1fafd63ee9a727e884666807618ac9bb9
prakrit-ti <prakrit.raj@trilogy.com>
2023-01-31 09:23:35 +0000

    possible exceptions enrich_lead.py

--- fast_api_als/services/enrich_lead.py
+++ fast_api_als/services/enrich_lead.py
@@ -10,6 +10,14 @@ from fast_api_als import constants
 what exceptions can be thrown here?
 """
 
-
 def get_enriched_lead_json(adf_json: dict) -> dict:
-    pass
+    try:
+        pass
+    except IOError:
+        print("Invalid Json or empty file")
+    except RuntimeError:
+        print("Unexpected Error")
+    except ValueError:
+        print("Decoding Json Failed")
+    except PermissionError:
+        print("permission denied")
